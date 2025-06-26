3ca4f86c3b4ccd66225a32f17bcc1a92af55481d
Elias Madera De Regil <elias.madera@uber.com>
2025-06-26 02:33:53 -0700

    test

---
+++ test14.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+string input;
+string output;
+unordered_map<char,int> seen;
+
+getline(cin,input);
+for(int i = 0; i < input.size(); ++i){
+    if (!++seen[input[i]]){
+        output.push_back(input[i]);
+    }
+}
+cout<<output;
+
+return 0;}
